4dce1195d8c4c6bea4086712a829842794722900
guicrisostomo <guilhermecrisostomo75@gmail.com>
2022-05-18 20:42:23 -0300

    Add: project theme switcher

---
+++ theme-switch/src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import { ThemeProvider } from 'styled-components';
+import TodoList from './components/TodoList'
+import ThemeSwitcher from './components/ThemeSwitcher'
+
+import * as themes from './styles/themes'
+import ThemeContext from './styles/themes/context'
+
+export default class App extends Component {
+  state = {
+    theme: themes.dark,
+  };
+
+  toggleTheme = () => {
+    this.setState({ theme: this.state.theme === themes.dark ? themes.light : themes.dark});
+  };
+
+  render() {
+    return (
+      <div>
+        <ThemeContext.Provider value={this.state}>
+          <ThemeSwitcher toggleTheme={this.toggleTheme} />
+          <ThemeContext.Consumer>
+            {theme => (
+              <ThemeProvider theme={theme}>
+                <TodoList />
+              </ThemeProvider>
+            )}
+          </ThemeContext.Consumer>
+        </ThemeContext.Provider>
+      </div>
+    )
+  }
+}
---
+++ theme-switch/src/components/TodoList/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import styled from 'styled-components';
+
+const List = styled.ul`
+    background: ${props => props.theme.theme.background};
+    color: ${props => props.theme.theme.color};
+    font-family: sans-serif;
+    font-size: 18px;
+
+    li {
+        line-height: 36px;
+    }
+`;
+
+const TodoList = () => (
+    <List>
+        <li>A</li>
+        <li>B</li>
+        <li>C</li>
+    </List>
+)
+
+export default TodoList;
